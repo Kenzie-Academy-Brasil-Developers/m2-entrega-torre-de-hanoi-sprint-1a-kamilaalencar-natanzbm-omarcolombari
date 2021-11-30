f66b02e3e3a75a0adc822201f8bb50e7120814ad
Natanael Matos <natanbmatos@gmail.com>
2021-11-30 19:07:08 -0300

    criar funçoes de movimento

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,82 +1,34 @@
-/* Constantes gerais para geração dos elementos usando o DOM*/
-/* Quadrados */
-const box1 = document.getElementById('space1');
-const box2 = document.getElementById('space2');
-const box3 = document.getElementById('space3');
+function createTower() {
+    const box = document.getElementById('box');
 
-/* Torres */
-const tower1 = document.createElement('div');
-const tower2 = document.createElement('div');
-const tower3 = document.createElement('div');
+    const tower1 = document.createElement('div');
+    const tower2 = document.createElement('div');
+    const tower3 = document.createElement('div');
 
-/* Discos */
-/* Tamanho dos discos começando do menor (1) ao maior (3) */
-const disk1 = document.createElement('div');
-const disk2 = document.createElement('div');
-const disk3 = document.createElement('div');
+    tower1.classList.add('tower1', 'tower');
+    tower2.classList.add('tower2', 'tower');
+    tower3.classList.add('tower3', 'tower');
 
-
-/* Funções para mostrar discos e torres */
-function showDisks() {
-    diskC(box1);
-    diskB(box1);
-    diskA(box1);
+    box.appendChild(tower1);
+    box.appendChild(tower2);
+    box.appendChild(tower3);
 }
-showDisks();
+createTower();
 
-function showTowers() {
-    leftTower(box1);
-    middleTower(box2);
-    rightTower(box3);
+
+function createDisc() {
+    const tower = document.getElementsByClassName('tower1')[0];
+
+    const disc1 = document.createElement('div');
+    const disc2 = document.createElement('div');
+    const disc3 = document.createElement('div');
+
+    disc1.classList.add('disc1', 'disc');
+    disc2.classList.add('disc2', 'disc');
+    disc3.classList.add('disc3', 'disc');
+
+    tower.appendChild(disc1);
+    tower.appendChild(disc2);
+    tower.appendChild(disc3);
 }
-showTowers();
-
-
-/* Funcionalidades e estilo usando o DOM */
-
-/* Torres */
-function leftTower(tower) {
-    tower.appendChild(tower1);
-    tower1.classList.add('torres');
-    return tower1;
-}
-
-function middleTower(tower) {
-    tower.appendChild(tower2);
-    tower2.classList.add('torres');
-    return tower2;
-}
-
-function rightTower(tower) {
-    tower.appendChild(tower3);
-    tower3.classList.add('torres');
-    return tower3;
-}
-
-/* Discos */
-function diskA(disc) {
-    disc.appendChild(disk1);
-    disk1.classList.add('discos');
-    disk1.style.width = '40%';
-    disk1.style.height = '40px';
-    disk1.style.backgroundColor = 'blue';
-    return disk1;
-}
-
-function diskB(disc) {
-    disc.appendChild(disk2);
-    disk2.classList.add('discos');
-    disk2.style.width = '60%';
-    disk2.style.height = '40px';
-    disk2.style.backgroundColor = 'darkblue';
-    return disk2;
-}
-
-function diskC(disc) {
-    disc.appendChild(disk3);
-    disk3.classList.add('discos');
-    disk3.style.width = '80%';
-    disk3.style.height = '40px';
-    disk3.style.backgroundColor = 'black';
-    return disk3;
-}
+createDisc();
